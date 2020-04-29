e988457b2c1dd82aea0255cf02daed354e350bba
maksmaggot <maksmaggot@gmail.com>
2020-04-29 15:28:45 +0700

    factor 9.Graceful shutdown

--- cmd/twelve/main.go
+++ cmd/twelve/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -15,9 +21,24 @@ func main() {
 		logrus.Fatal("PORT is not set")
 	}
 
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusOK)
 	})
 
-	http.ListenAndServe(":"+port, nil)
+	serv := http.Server{
+		Addr:    net.JoinHostPort("", port),
+		Handler: router,
+	}
+	go serv.ListenAndServe()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	<-interrupt
+
+	timeout, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	serv.Shutdown(timeout)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/maksmaggot/twelve
 
 go 1.14
 
-require github.com/sirupsen/logrus v1.5.0
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/sirupsen/logrus v1.5.0
+)
